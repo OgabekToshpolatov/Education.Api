084725089c34d5b2b53446b1bda1a7eeb1449a95
Ogabek <ogabektoshpolatov2002@gmail.com>
2022-11-25 03:07:47 +0500

    Add LOcalizationProvider

--- Classroom/Models/CreateCourceDto.cs
+++ Classroom/Models/CreateCourceDto.cs
@@ -1,9 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Classroom.Models;
 
 public class CreateCourceDto
 {
-    [Required]
+    [Required(ErrorMessage = "Required")]
+
     public string? Name { get; set; }
+
+    public CreateCourceDto()
+    {
+        var culture = CultureInfo.CurrentCulture;
+    }
 }
--- Classroom/Program.cs
+++ Classroom/Program.cs
@@ -1,13 +1,21 @@
+using System.Globalization;
 using Classroom.Context;
 using Classroom.Entities;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddDataAnnotationsLocalization(options => {
+      options.DataAnnotationLocalizerProvider = 
+            (type ,factory) => factory.Create(typeof(Program));
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddLocalization(options => {
+  options.ResourcesPath = "Resources";
+}); 
 
 builder.Services.AddDbContext<AppDbContext>(options => 
 {
@@ -33,6 +41,22 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRequestLocalization(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture(new CultureInfo("Ru"));
+    options.SupportedUICultures = new List<CultureInfo>()
+    {
+        new CultureInfo("Ru"),
+        new CultureInfo("Uz"),
+        new CultureInfo("En"),
+    };
+    options.SupportedCultures = new List<CultureInfo>()
+    {
+        new CultureInfo("Ru"),
+        new CultureInfo("Uz"),
+        new CultureInfo("En"),
+    };
+});
 
 app.UseAuthentication();
 
